f36057545db0e27d6348eb474ea7004e0949ac68
AbedinTelaku <abedintelaku1@gmail.com>
2023-11-29 14:03:24 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the Course'},
+    {id: 'cg1', text: 'Finish the Course'},
+    {id: 'cg1', text: 'Finish the Course'},
+  ]
+  return (
+    
+  );
+}
+
+export default App;
