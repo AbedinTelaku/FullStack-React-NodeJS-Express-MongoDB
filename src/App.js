5fc93ea95c4b5ce1ef0cb528951156e4c4f14b26
AbedinTelaku <abedintelaku1@gmail.com>
2023-12-02 22:32:51 +0100

    Added users static

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom
 import NewPlace from './places/pages/NewPlace';
 import Users from './user/pages/Users';
 import MainNavigation from "./shared/components/Navigation/MainNavigation"
+import UserPlaces from './places/pages/UserPlaces'
 
 const App = () => {
   return (
@@ -13,6 +14,9 @@ const App = () => {
       <Route path="/" exact>
         <Users/>
       </Route>
+      <Route path="/:userId/places" exact>
+        <UserPlaces/>
+        </Route>
       <Route path="/places/new" exact>
         <NewPlace/>
       </Route>
