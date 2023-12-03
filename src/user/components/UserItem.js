db953e76fb1f9efdde394561f0cdf8b58c64bb98
AbedinTelaku <abedintelaku1@gmail.com>
2023-12-03 16:01:40 +0100

    Fix Bugs

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -5,21 +5,23 @@ import Card from '../../shared/components/UIElement/Card';
 import Avatar from "../../shared/components/UIElement/Avatar";
 
 const UserItem = props => {
-  return(
-    <li className='user-item'>
-        <Card className='user-item__content'>
+  return (
+    <li className="user-item">
+      <Card className="user-item__content">
         <Link to={`/${props.id}/places`}>
-        <div className='user-item__image'>
-          <Avatar image={props.image} alt={props.name}/>
-        </div>
-        <div className='user-item__info'>
+          <div className="user-item__image">
+            <Avatar image={props.image} alt={props.name} />
+          </div>
+          <div className="user-item__info">
             <h2>{props.name}</h2>
-            <h3>{props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}</h3>
-        </div>
+            <h3>
+              {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+            </h3>
+          </div>
         </Link>
-        </Card>
+      </Card>
     </li>
-  )
-}
+  );
+};
 
 export default UserItem;
